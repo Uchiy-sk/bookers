cc90c4a9239baced940a344096649a82b9c84e32
EC2 Default User <ec2-user@ip-172-31-33-119.ec2.internal>
2022-10-08 05:51:46 +0000

    バリテーション機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,30 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
+
+  # book.update(book_params)
+  # redirect_to book_path(book.id)
 
   def destroy
     book = Book.find(params[:id])
